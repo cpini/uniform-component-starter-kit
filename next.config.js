d2d3508189939553c2cbca4a63d5aca9bfadd3db
Cris Pini <cris_pini@yahoo.co.uk>
2024-11-04 17:51:55 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version,
+    NEXT_PUBLIC_BUILD_TIMESTAMP: String(new Date().valueOf()),
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '*' }],
+    deviceSizes: [320, 420, 640, 768, 1024, 1280, 1536],
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
